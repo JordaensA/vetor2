b74668486ce3e4a5a1aefbfd5aa9c18618748a23
Jordaens Aristil <jordaens.aristil@gmail.com>
2020-06-07 17:45:29 -0300

    commit projeto vetor

---
+++ ExercicioVetores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudade[] vect = new Estudade[10];
+
+            Console.WriteLine("Entre com o numero dos quartos:  ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Aluguel #" + i);
+                Console.Write("nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int qtd = int.Parse(Console.ReadLine());
+                vect[qtd] = new Estudade(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i+ ": "+ vect[i]);
+                }
+            }
+
+
+        }
+    }
+}
+
+            
+
+
+
+
+
+        
+    
+
---
+++ Parametro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Parametro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           int c1 = Calculator.Sum(2,3);
+           int c2 = Calculator.Sum(4,5,6,8);
+
+
+            Console.WriteLine(c1);
+            Console.WriteLine( c2 );
+        }
+    }
+}
